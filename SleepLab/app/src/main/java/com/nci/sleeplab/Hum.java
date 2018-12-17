7e736e40cac5d158caf4ddb9a2928da5c1c6459c
hannah <hannahm1234@hotmail.com>
2018-12-17 16:28:33 +0000

    Humidty values displaying

--- SleepLab/app/src/main/java/com/nci/sleeplab/Hum.java
+++ SleepLab/app/src/main/java/com/nci/sleeplab/Hum.java
@@ -2,13 +2,48 @@ package com.nci.sleeplab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Hum extends AppCompatActivity {
 
-    //Hum file
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hum);
+    //sound view
+        private static final String TAG = "Hum";
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_hum);
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRefTemp = database.getReference("Hum");
+
+
+            final TextView myText = findViewById(R.id.textHum);
+
+
+            // Read from the database
+            myRefTemp.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    myText.setText(dataSnapshot.getValue().toString());
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+
+        }
+
     }
-}
